5701ec1773566422cccaed3b8a67d30d5714fc20
Giroik <ervinaz@inbox.lv>
2022-05-14 19:51:41 +0200

    Promille kalkulator Fixed some "bags" changed design and functional part of program

--- Script.js
+++ Script.js
@@ -3,17 +3,24 @@ AllDrinks={
     pils:["lettol",0.045],
     rusbrus:["lettol",0.047],
     vin:["lettol",0.12],
-    vodka:["lettol",0.40]
+    vodka:["lettol",0.40],
+    anetLiter:["anetLiter",0]
 }
-
-
 let sumertPromiler=0
 document.querySelector("#Tele").onclick = function(evn)
 {
-    let Names=["lettol","pils","rusbrus","vin","vodka"];
+    if(document.getElementById("vekt").value==0)
+    {
+        alert("Skriv inn vekt")
+        return;
+    }
+    document.getElementById("resInfo").innerHTML=""
+    let Names=["lettol","pils","rusbrus","vin","vodka","anetLiter"];
+    AllDrinks["anetLiter"][1]=Number(document.getElementById("anet").value/100).toFixed(2)
     let gram=0
     for(let i=0; i < Names.length;i++)
     {
+        console.log(document.getElementById(Names[i]).value,AllDrinks[Names[i]][1])
         gram+=Number(document.getElementById(Names[i]).value)*AllDrinks[Names[i]][1]*0.78
     }
     gram=gram*1000
@@ -39,6 +46,12 @@ document.querySelector("#Tele").onclick = function(evn)
     {
         antalHalvtimer=((24*60-min1)+min2)/30
     }
+
+    if(min2-min1==0)
+    {
+        alert("den minste defiransen må være 30 minuter")
+        return;
+    }
     //////
 
     let konstativTal=0
@@ -47,11 +60,34 @@ document.querySelector("#Tele").onclick = function(evn)
     {
         sumertPromiler=((gram/(Number(document.getElementById("vekt").value)*0.7))-(0.15*0.5*antalHalvtimer))
         konstativTal=0.7
+        if(sumertPromiler<0)
+        {
+            document.getElementById("resInfo").innerHTML="<h2>Alkohol er burte</h2>"
+        }
+
     }
     else if(document.getElementById("Femail").checked)
     {
         sumertPromiler=((gram/(Number(document.getElementById("vekt").value)*0.6))-(0.15*0.5*antalHalvtimer))
         konstativTal=0.6
+        if(sumertPromiler<0)
+        {
+            document.getElementById("resInfo").innerHTML="<h2>Alkohol er burte</h2>"
+        }
+    }
+    else
+    {
+        alert("du må velge 'Male' eller 'Femail'")
+        return;
+    }
+    if(sumertPromiler>4)
+    {
+        alert("dø dose av alkohol er 4-6 promiller, du har: "+sumertPromiler+" du er i risiko")
+        if(sumertPromiler>8)
+        {
+            alert("det er ikke mulig å leve med sån dose av alkohol")
+            return;
+        }
     }
     let sumertPromiler2=0
     let a=0
@@ -62,37 +98,34 @@ document.querySelector("#Tele").onclick = function(evn)
         a++
         if(antalHalvtimer>=a)
         {
-
             sumertPromiler=(a/antalHalvtimer)*((gram/(Number(document.getElementById("vekt").value)*konstativTal))-(0.15*0.5*antalHalvtimer))
+            console.log(sumertPromiler)
         }
         else if(antalHalvtimer<a)
         {
             sumertPromiler=((gram/(Number(document.getElementById("vekt").value)*konstativTal))-(0.15*0.5*a))
+            console.log(sumertPromiler)
+
         }
-
-
         ///////////Skriv ut
-
         let timerr=(min1/60).toFixed(0)
         let minuter=((min1+30)%60)
         min1+=30
 
-
-        if(timerr==24&& minuter==0)
+        if(min1>=60*24)
         {
             min1=0
             timerr=0
-            minuter=0
+            minuter=minuter%30
         }
-
         if(minuter==0)
         {
-            minuter="00"
+            minuter="00";
         }
         let divEl=document.createElement("div");
         let pEl=[document.createElement("p"),document.createElement("p"),document.createElement("p")]
 
-        let trust
+        let trust=""
         if(sumertPromiler>sumertPromiler2)
         {
             trust="Opp"
@@ -100,10 +133,6 @@ document.querySelector("#Tele").onclick = function(evn)
         if(sumertPromiler<sumertPromiler2)
         {
             trust="Ned"
-        }
-        if(sumertPromiler==sumertPromiler2)
-        {
-            trust="Lik"
         }
         let allInfo
         if(sumertPromiler>=0) {
@@ -122,7 +151,6 @@ document.querySelector("#Tele").onclick = function(evn)
             divEl.className="infoDiv"
         }
         document.getElementById("resInfo").appendChild(divEl)
-
     }
 }
 
